0f917577b2305e60fe750908e3198a3c689fcafc
Travis Rodgers <travisdotmedia@gmail.com>
2023-05-11 19:10:52 -0400

    deleteDoc functionality

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -6,7 +6,7 @@ import Table from './Table';
 import Add from './Add';
 import Edit from './Edit';
 
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, doc, deleteDoc } from "firebase/firestore";
 import { db } from '../../config/firestore'
 
 const Dashboard = ({ setIsAuthenticated }) => {
@@ -44,7 +44,7 @@ const Dashboard = ({ setIsAuthenticated }) => {
       if (result.value) {
         const [employee] = employees.filter(employee => employee.id === id);
 
-        // TODO delete document
+        deleteDoc(doc(db, "employees", id));
 
         Swal.fire({
           icon: 'success',
